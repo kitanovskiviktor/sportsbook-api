0cd54bc1ca61301cd4f7f6934b6e079ff801d531
Viktor Kitanovski <viktor.kitanovski45@gmail.com>
2026-08-10 20:51:51 +0200

    Initial Commit

---
+++ src/main/java/com/sportsbook/SportsbookApiApplication.java
@@ -0,0 +1,13 @@
+package com.sportsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsbookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsbookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportsbook/SportsbookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportsbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsbookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
